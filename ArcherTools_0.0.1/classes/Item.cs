23d39c272f094216d106f4732c56591cd9978a93
Archer <luis.maia@etlogisticsinc.com>
2025-01-07 18:09:59 -0300

    Item class beginning of implementation.

---
+++ ArcherTools_0.0.1/classes/Item.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcherTools_0._0._1.classes
+    {
+    internal class Item
+    {
+        public string owner {  get; set; }
+        public string style {  get; set; }
+        public string itemCode { get; set; }
+        public string color {  get; set; }
+        public string colorCode { get; set; }
+        public string size { get; set; }
+        public string description { get; set; }
+
+        internal Item(string style, string itemCode = "", string color = "", string colorCode = "", string size = "", string description = "", string owner = "") 
+        {
+            this.owner = owner;
+            this.style = style;
+            this.itemCode = itemCode;
+            this.color = color;
+            this.colorCode = colorCode;
+            this.size = size;
+            this.description = description;
+
+        }
+
+        public string getStyleFromCode(string code)
+        {            
+           string [] splitItem =  code.Split(new char[] {'-', '\n', ' '}, StringSplitOptions.RemoveEmptyEntries);
+           return splitItem[0];
+        }
+
+        public static bool compareSizes(string size1, string size2)
+        {
+            try
+            {
+                if (size1.Equals(size2))
+                {
+                    return true;
+                }
+            }
+            catch (Exception e) {
+                Debug.WriteLine(e.Message);                
+            }
+            return false;
+        }
+    }
+}
---
+++ ArcherTools_0.0.1/enum_things/ItemSizes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcherTools_0._0._1.enum_things
+{
+    public enum ItemSizes
+    {
+        [StringValue("3XS")]
+        XXXS,
+        [StringValue("2XS")]
+        XXS,
+        [StringValue("1XS")]
+        XS,
+        [StringValue("S")]
+        S,
+        [StringValue("M")]
+        M,
+        [StringValue("L")]
+        L,
+        [StringValue("1XL")]
+        XL,
+        [StringValue("2XL")]
+        XXL,
+        [StringValue("3XL")]
+        XXXL,
+
+    }
+}
